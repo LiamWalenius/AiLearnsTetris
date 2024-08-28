eedfa12ad620d89ba255246d4a35a90a16f82483
LiamWalenius <liamwalenius@gmail.com>
2024-08-28 19:46:36 -0400

    add piece movement, walls, floor, and collision

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 import tetris
+import sys
+import colours
 
 def main():
     pygame.init()
@@ -7,7 +9,29 @@ def main():
     pygame.display.set_caption('Tetris')
     frame_counter = pygame.time.Clock()
     frame_counter.tick(60)
-    update_time = 250
+    update_time = 500
     update_event = pygame.event.custom_type()
     pygame.time.set_timer(update_event, update_time)
     game = tetris.Tetris()
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            if event.type == update_event:
+                game.update()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_a:
+                    game.active_piece.move_left()
+                elif event.key == pygame.K_d:
+                    game.active_piece.move_right()
+                elif event.key == pygame.K_r:
+                    game.active_piece.rotate()
+
+        surf.fill(colours.BLACK)
+        game.draw(surf)
+        pygame.display.update()
+
+
+if __name__ == '__main__':
+    main()
--- tetris.py
+++ tetris.py
@@ -8,9 +8,12 @@ class Position:
     r: int
     c: int
 
-@dataclass(frozen=True)
 class Shape:
-    _data: list[list[bool]]
+    def __init__(self, data: list[list[int]]):
+        self._data = [[bool(i) for i in row] for row in data]
+
+    def __iter__(self):
+        return iter(self._data)
 
     def get(self, r: int, c: int) -> bool:
         return self._data[r][c]
@@ -19,13 +22,16 @@ class Piece:
     def __init__(self, shapes: list[Shape]) -> None:
         self.shapes = shapes
         self.active_shape_ind = 0
-        self.pos = Position(0, 0)
+        self.pos = Position(0, 1)
 
     def rotate(self) -> None:
         self.active_shape_ind = (self.active_shape_ind + 1) % len(self.shapes)
 
-    def move_down(self) -> None:
+    def move_up(self) -> None:
         self.pos.r -= 1
+
+    def move_down(self) -> None:
+        self.pos.r += 1
 
     def move_left(self) -> None:
         self.pos.c -= 1
@@ -42,26 +48,64 @@ class GridNode(Enum):
     BLOCK = 2
 
 class Tetris:
-    node_colours = {
+    NODE_COLOURS = {
         GridNode.EMPTY: colours.BLACK,
         GridNode.WALL: colours.WHITE,
         GridNode.BLOCK: colours.RED,
     }
 
+    # 10 empty spaces + 2 for the walls
+    GRID_WIDTH = 10 + 2
+    # 20 empty spaces + 1 for the floor
+    GRID_HEIGHT = 20 + 1
+
     def __init__(self) -> None:
         self.pieces = list[Piece]()
+        self.pieces.append(Piece([
+            Shape([
+                [0, 1, 0, 0],
+                [0, 1, 0, 0],
+                [0, 1, 0, 0],
+                [0, 1, 0, 0]
+            ]),
+            Shape([
+                [0, 0, 0, 0],
+                [1, 1, 1, 1],
+                [0, 0, 0, 0],
+                [0, 0, 0, 0]
+            ])
+        ]))
         self.active_piece = self.pieces[0]
-        self.grid = list[list[GridNode]]()
+        self.grid = [[GridNode.EMPTY for _ in range(Tetris.GRID_WIDTH)] for _ in range(Tetris.GRID_HEIGHT)]
+        for i in range(Tetris.GRID_HEIGHT):
+            self.grid[i][0] = self.grid[i][-1] = GridNode.WALL
+        self.grid[-1] = [GridNode.WALL for _ in range(Tetris.GRID_WIDTH)]
 
     def update(self) -> None:
         self.active_piece.move_down()
+
+        for r, row in enumerate(self.active_piece.get_active_shape()):
+            for c, val in enumerate(row):
+                if not val:
+                    continue
+
+                grid_r = self.active_piece.pos.r + r
+                grid_c = self.active_piece.pos.c + c
+
+                if self.grid[grid_r][grid_c] != GridNode.EMPTY:
+                    self.active_piece.move_up()
+                    self.make_active_piece_blocks()
+                    return
+
+    def make_active_piece_blocks(self) -> None:
+        pass
 
     def rotate_active_piece(self) -> None:
         self.active_piece.rotate()
 
     def draw(self, surf: pygame.surface.Surface) -> None:
         width, height = surf.get_size()
-        node_size = min(width // len(self.grid[0]), height // len(self.grid))
+        node_size = min(width // Tetris.GRID_WIDTH, height // Tetris.GRID_HEIGHT)
 
         for r, row in enumerate(self.grid):
             for c, node in enumerate(row):
@@ -72,4 +116,18 @@ class Tetris:
                     node_size
                 )
 
-                pygame.draw.rect(surf, Tetris.node_colours[node], draw_rect)
+                pygame.draw.rect(surf, Tetris.NODE_COLOURS[node], draw_rect)
+
+        for r, row in enumerate(self.active_piece.get_active_shape()):
+            for c, node in enumerate(row):
+                if not node:
+                    continue
+
+                draw_rect = pygame.Rect(
+                    (self.active_piece.pos.c + c) * node_size,
+                    (self.active_piece.pos.r + r) * node_size,
+                    node_size,
+                    node_size
+                )
+
+                pygame.draw.rect(surf, colours.RED, draw_rect)
